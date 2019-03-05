f03b793c1d07f669a8ecd9bb4fed3de1c85b8b6a
Max Roald Eckardt <max@nodeCortex.com>
2019-03-05 14:22:15 +0100

    Revert "got rid of code"
    This reverts commit b9285be844a9f7c11291d70dfd6942387490c520.

---
+++ example.py
@@ -0,0 +1,35 @@
+import sys
+
+def subtract(x, y):
+    """
+    subtracts y from x
+    """
+    return x - y
+
+def add(x, y):
+    """
+    Returns the sum of the two arguments.
+    """
+    return x + y
+
+
+def another_function(x):
+    """
+    Returns square of the argument.
+    """
+    return x * x
+
+
+def test_another_function():
+    assert another_function(3.0) == 9.0
+
+
+if __name__ == '__main__':
+
+    # there are better ways to pass command line arguments
+    # to a python code but this is not the point here
+    x = float(sys.argv[-2])
+    y = float(sys.argv[-1])
+
+    result = simple_function(x, y)
+    print(result)
